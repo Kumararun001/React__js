7787e04a54488fac8024eb9a14ce2965f7866384
Kumararun001 <kumararun25398@gmail.com>
2022-09-28 12:04:47 +0530

    update my react js project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+// import profile from "./Profile.js";
+//  import Comp from "./Funct.js";
+// import State from "./state.js";
+// import State from "./states.js";
+// import Toggle from "./toggle";
+// import Test from "./testprop";
+import Temp from "./component/temp"
+
+function App() {
+  return (
+    <div>
+     <Temp/>
+    </div>
+)}
+
+export default App;
+
+
+// 657f7ec39572a3968dcde83f7ed9f19a
+// https://api.openweathermap.org/data/2.5/weather?q=pune&appid=
---
+++ src/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class profile extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a normal class component</h1>
+      </div>
+    );
+  }
+}
+export default profile;
